c9dfa0b678f69cb007a643d34c407f6676626822
win <1277615541@qq.com>
2019-01-22 18:18:52 +0800

    萧毅

--- cmd/时间.go
+++ cmd/时间.go
@@ -6,19 +6,44 @@ import (
 	"time"
 )
 
-const base_format = "2006-01-02 15:04:05"
+const BASE_FORMAT = "2006-01-02"
 
 func main() {
 
-	t1 := time.Date(2018, 1, 10, 0, 0, 1, 100, time.Local)
-	t2 := time.Date(2018, 2, 9, 23, 59, 22, 100, time.Local)
-	fmt.Println(timeSub(t1, t2))
-	s := t1.Format(base_format)
-	s_time, _ := time.Parse(base_format, s)
+	//t1 := time.Date(2019, 1, 6, 0, 0, 1, 100, time.Local)
+	//t2 := time.Date(2019, 1, 20, 23, 59, 22, 100, time.Local)
+	fmt.Println(workerDay("2019-01-06", "2019-01-20"))
 
-	fmt.Println(s, "=============之前的论剑时间小时是")
-	fmt.Println(s_time.Weekday())
+	//s := t1.Format(BASE_FORMAT)
+	//s_time, _ := time.Parse(BASE_FORMAT, s)
+	//
+	//fmt.Println(s, "=============之前的论剑时间小时是")
+	//fmt.Println(t2.Weekday())
+	//fmt.Println(s_time.Weekday())
 }
+
+/**
+  根据日期计算工作日
+*/
+func workerDay(s, e string) int64 {
+	start, _ := time.Parse(BASE_FORMAT, s)
+	end, _ := time.Parse(BASE_FORMAT, e)
+	var count int64
+	var d int64
+	start = time.Date(start.Year(), start.Month(), start.Day(), 0, 0, 0, 0, time.Local)
+	end = time.Date(end.Year(), end.Month(), end.Day(), 0, 0, 0, 0, time.Local)
+	day := int64(math.Abs(float64(start.Sub(end).Hours() / 24)))
+	for d = 0; d <= day; d++ {
+		s := start.AddDate(0, 0, int(d)).Weekday().String()
+		if "Sunday" == s || "Saturday" == s {
+			fmt.Println(s)
+			continue
+		}
+		count++
+	}
+	return count
+}
+
 func timeSub(t1, t2 time.Time) int64 {
 	t1 = time.Date(t1.Year(), t1.Month(), t1.Day(), 0, 0, 0, 0, time.Local)
 	t2 = time.Date(t2.Year(), t2.Month(), t2.Day(), 0, 0, 0, 0, time.Local)
