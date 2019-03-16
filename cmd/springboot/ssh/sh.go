278c1c8af560971641f625a23c5c83adf49c5d06
pc <14641860577@qq.com>
2019-03-16 12:44:24 +0800

    萧毅

---
+++ cmd/springboot/ssh/sh.go
@@ -0,0 +1,51 @@
+package main
+
+const (
+
+	//主目录
+	Paths ="/root/test"
+	//文件夹名称
+	Dir	=	"file"
+	//jar包名
+	Jar	=	"file-0.0.1-SNAPSHOT.jar"
+
+)
+
+
+
+func main() {
+
+	//1，判断文件夹是否存在
+	//1.1不存在
+	//1.1.2 获取文件
+	//打包
+	//运行
+	//1.2存在
+	//1.2.1 更新文件
+	//打包
+	//运行
+
+}
+
+//判断文件夹是否存在
+func dirIsExist()  {
+
+
+	path.
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
